8d7c680862e7c5302015dd134ac67fb8f4eab5a6
Chloe Chen <chloechen408@gmail.com>
2021-09-27 13:29:51 -0400

    5.6 complete, structs

---
+++ ToDoList/ToDoItem.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoItem.swift
+//  ToDoList
+//
+//  Created by Chloe Chen on 9/27/21.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
