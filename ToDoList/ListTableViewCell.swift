57431268fc608cb7d66d6e4cbb78317b39485930
Chloe Chen <chloechen408@gmail.com>
2021-10-04 15:10:38 -0400

    finished 5.11, add checkmarks

---
+++ ToDoList/ListTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ListTableViewCell.swift
+//  ToDoList
+//
+//  Created by Chloe Chen on 10/4/21.
+//
+
+import UIKit
+
+protocol ListTableViewCellDelegate: class {
+    func checkBoxToggle(sender: ListTableViewCell)
+}
+
+class ListTableViewCell: UITableViewCell {
+    
+    weak var delegate: ListTableViewCellDelegate?
+
+    @IBOutlet weak var checkBoxButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    @IBAction func checkToggled(_ sender: UIButton) {
+        delegate?.checkBoxToggle(sender: self)
+    }
+}
